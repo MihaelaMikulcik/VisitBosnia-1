094b9ced043c86c3dd42831d9f25166c7bb17b84
MihaelaMikulcik <mihaela.mikulcik@edu.fit.ba>
2022-07-20 23:05:41 +0200

    merge

--- VisitBosnia/VisitBosnia/Controllers/CityContoller.cs
+++ VisitBosnia/VisitBosnia/Controllers/CityContoller.cs
@@ -1,15 +1,29 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using VisitBosnia.Model.Requests;
 using VisitBosnia.Services.Interfaces;
 
 namespace VisitBosnia.Controllers
 {
-    [AllowAnonymous]
+   
     public class CityController : BaseCRUDController<Model.City, CitySearchObject, CityInsertRequest, CityUpdateRequest>
     {
-        public CityController(ICityService service):base(service)
+        public CityController(ICityService service) : base(service)
         {
 
+        }
+
+        //[HttpGet]
+        //public string GetName()
+        //{
+        //    return "Ime test";
+        //}
+
+        [HttpGet("GetName2")]
+        public async Task<IActionResult> GetName2()
+        {
+            var result = await base.GetById(1);
+            return Ok(result.Name);
         }
     }
 }
