f7223783f0bf96d7a99deb466db7d6b94ef24d3e
NudzejmaDedovic <107313619+NudzejmaDedovic@users.noreply.github.com>
2022-08-04 00:45:30 +0200

    Forum page

--- VisitBosnia/visit_bosnia_mobile/lib/pages/forum.dart
+++ VisitBosnia/visit_bosnia_mobile/lib/pages/forum.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:provider/provider.dart';
+import 'package:visit_bosnia_mobile/components/navigation_drawer.dart';
 import 'package:visit_bosnia_mobile/providers/city_provider.dart';
+import 'package:visit_bosnia_mobile/utils/util.dart';
 
 import '../model/city/city.dart';
 
@@ -17,6 +19,7 @@ class Forum extends StatefulWidget {
 class _ForumState extends State<Forum> {
   late CityProvider _cityProvider;
   // List<City> _cities = [];
+  String? search = "";
 
   @override
   void initState() {
@@ -26,20 +29,26 @@ class _ForumState extends State<Forum> {
   }
 
   Future<List<City>> GetData() async {
-    var cities = await _cityProvider.get();
+    List<City> cities;
+    if (search != "") {
+      cities = await _cityProvider.get({'Name': search});
+    } else {
+      cities = await _cityProvider.get();
+    }
     return cities;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        drawer: NavigationDrawer(),
         appBar: AppBar(
-          backgroundColor: Color.fromRGBO(29, 76, 120, 1),
+          backgroundColor: const Color.fromRGBO(29, 76, 120, 1),
         ),
         body: Column(
           children: [
-            Container(
-              height: MediaQuery.of(context).size.height * 0.18,
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.16,
               width: MediaQuery.of(context).size.width,
               child: Stack(
                 clipBehavior: Clip.none,
@@ -65,7 +74,7 @@ class _ForumState extends State<Forum> {
                     left: 0,
                     right: 0,
                     child: Container(
-                      margin: EdgeInsets.only(
+                      margin: const EdgeInsets.only(
                         left: 40,
                         right: 40,
                       ),
@@ -80,20 +89,27 @@ class _ForumState extends State<Forum> {
                                 blurRadius: 10,
                                 color: Colors.grey)
                           ]),
-                      child: const TextField(
-                          decoration: InputDecoration(
-                              fillColor: Colors.red,
-                              border: InputBorder.none,
-                              hintText: "Search cities...",
-                              suffixIcon: Icon(Icons.search),
-                              contentPadding: EdgeInsets.symmetric(
-                                  horizontal: 10, vertical: 8))),
+                      child: TextField(
+                        decoration: const InputDecoration(
+                            border: InputBorder.none,
+                            hintText: "Search cities...",
+                            suffixIcon: Icon(Icons.search),
+                            contentPadding: EdgeInsets.symmetric(
+                                horizontal: 10, vertical: 8)),
+                        onChanged: (value) {
+                          print(value);
+                          setState(() {
+                            search = value;
+                          });
+                          // GetData(value);
+                        },
+                      ),
                     ),
                   )
                 ],
               ),
             ),
-            SizedBox(height: 50),
+            const SizedBox(height: 50),
             Expanded(
               // height: 300,
               child: _buildCityList(),
@@ -107,20 +123,20 @@ class _ForumState extends State<Forum> {
         future: GetData(),
         builder: (BuildContext context, AsyncSnapshot<List<City>> snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(
+            return const Center(
               child: CircularProgressIndicator(),
               // child: Text('Loading...'),
             );
           } else {
             if (snapshot.hasError) {
-              return Center(
+              return const Center(
                 // child: Text('${snapshot.error}'),
                 child: Text('Something went wrong...'),
               );
             } else {
               return ListView(
                 scrollDirection: Axis.vertical,
-                physics: ScrollPhysics(),
+                physics: const ScrollPhysics(),
                 children: snapshot.data!.map((e) => cityWidget(e)).toList(),
               );
             }
@@ -132,11 +148,24 @@ class _ForumState extends State<Forum> {
     return InkWell(
       onTap: () {},
       child: Container(
-        padding: EdgeInsets.all(30),
-        margin: EdgeInsets.only(left: 30, bottom: 13, right: 30),
+        height: 85,
+        padding: const EdgeInsets.all(30),
+        margin: const EdgeInsets.only(left: 30, bottom: 13, right: 30),
         decoration: BoxDecoration(
-            color: Colors.amber, borderRadius: BorderRadius.circular(10)),
-        child: Text(city.name!),
+          borderRadius: BorderRadius.circular(10),
+          image: DecorationImage(
+            image: imageFromBase64String(city.image!).image,
+            fit: BoxFit.cover,
+            colorFilter: ColorFilter.mode(
+                Colors.black.withOpacity(0.5), BlendMode.darken),
+          ),
+        ),
+        child: Text(
+          city.name!,
+          textAlign: TextAlign.center,
+          style: const TextStyle(
+              fontSize: 25, color: Colors.white, fontWeight: FontWeight.bold),
+        ),
       ),
     );
   }
